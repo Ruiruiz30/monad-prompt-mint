15568896092254f61c6fdb34e337d8ff4bf017bb
dsy <unlcedou@qq.com>
2025-08-03 11:56:40 +0800

    fix: 修复OperationHistory组件中useEffect未定义的错误 - 添加useEffect到React导入中

--- src/components/OperationHistory.tsx
+++ src/components/OperationHistory.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useAppContext, OperationHistoryItem } from '@/contexts/AppContext'
 import { formatDistanceToNow } from 'date-fns'
 
